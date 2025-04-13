42fa918fae32b5eb3e889cf6c8629453c77ea29b
Adrian Šiška <adrian@computers.si>
2025-04-13 07:48:45 +0200

    Its 6 in the evening on a saturday

---
+++ tside-bill/cat.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+var shit_is_going_down:int = 0
+var speed: float = 200
+
+func _on_cat_zone_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		shit_is_going_down = 1
+		var spawner = body.find_children("objectSpawner")[0].get_children()
+		for x in spawner:
+			x.remove_child(x)
+
+func _process(delta: float) -> void:
+	if shit_is_going_down == 1:
+		%rig.play("left")
+		shit_is_going_down = 2
+	if shit_is_going_down == 2:
+		self.position += Vector2(-1,0) * delta * speed;
