773bb52566c573c1f2c348641d0e20ad8fdce628
laraanzur <lara.anzur@gmail.com>
2025-04-13 04:42:02 +0200

    added classroom

---
+++ tside-bill/nakedguy.gd
@@ -0,0 +1,51 @@
+extends CharacterBody2D
+
+var target_node
+var blackbox
+var full_text
+var current_text = ""
+var has_interacted = false
+
+
+func _ready():
+	# Directly access the node using the full path
+	target_node = get_node("/root/SkretRoom/textbox/CanvasLayer/ColorRect/HBoxContainer/Label")
+	blackbox = get_node("/root/SkretRoom/textbox/CanvasLayer")
+	#target_node.visible = false
+	blackbox.visible = false
+
+func interact():
+	if not has_interacted:
+		blackbox.visible = true
+		#target_node.text = "changed text"
+		full_text = "This is the new animated text! I need to generate a really\nlong text so that i can test this functionality itd itd"
+		
+		type_text(full_text)
+		has_interacted = true
+	else:
+		blackbox.visible = true
+		full_text = "Did you bring anything useful?"
+		type_text(full_text)
+
+func type_text(full_text):
+	current_text = ""
+	target_node.text = current_text
+	
+	var typing_timer = Timer.new()
+	add_child(typing_timer)
+	typing_timer.wait_time = 0.05
+	typing_timer.one_shot = false
+	typing_timer.connect("timeout", func():
+		if current_text.length() < full_text.length():
+			current_text += full_text[current_text.length()]
+			target_node.text = current_text
+		else:
+			typing_timer.stop()
+			typing_timer.queue_free()
+	)
+	typing_timer.start()
+	#typing_timer.wait_time = 1.00
+	
+
+func going():
+	blackbox.visible = false
